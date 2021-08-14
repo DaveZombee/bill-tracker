bf1f6d0893ec34f735498c379d8c65e84044e4b4
DaveZombee <20luky301@gmail.com>
2021-08-14 20:42:36 +0800

    Update MainGUI.java

--- MainGUI.java
+++ MainGUI.java
@@ -3,10 +3,18 @@ import net.miginfocom.swing.MigLayout;
 
 import java.awt.Font;
 import java.awt.event.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
 
 public class MainGUI {
 
 	private JFrame frame = new JFrame("Bill tracker");
+	
+	private LinkedList<Bill> listOfBills = retrieveFromFile(); // deserializes linkedlist
 
 	// Navigation bar buttons
 	private JButton OVButton = new JButton("Overview");
@@ -14,7 +22,7 @@ public class MainGUI {
 	private JButton SettButton = new JButton("Settings");
 
 	// Creating two GUIs which allows for validation
-	private AddNewGUI addNewGUI = new AddNewGUI();
+	private AddNewGUI addNewGUI = new AddNewGUI(listOfBills);
 	private SettGUI settGUI = new SettGUI();
 
 	private int panel = 0; // also for validation - to see which panel is currently visible
@@ -27,13 +35,19 @@ public class MainGUI {
 
 		NavBar();
 		OVButton.setEnabled(false); // can't click overview button when starting
-
-		frame.add(new OverviewGUI().getPanel(), "span"); // Starting GUI, when program is first opened
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new OverviewGUI(listOfBills).getPanel(), "span"); // Starting GUI, when program is first opened
+		
 		frame.pack();
 		frame.setVisible(true);
-
+		
+		// Action taken when window is closed
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent) {
+				saveList();  // Serializes list
+				System.exit(0);
+			}
+		});
+		
 		/////////////////////////////
 		// BUTTON ACTION LISTENERS //
 		/////////////////////////////
@@ -42,8 +56,7 @@ public class MainGUI {
 		OVButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (addNewValidation()) { // if add new doesn't have empty fields
-					buttonClicked(new OverviewGUI().getPanel());
-					unclickableButton(OVButton, AddNewButton, SettButton);
+					buttonClicked(new OverviewGUI(listOfBills).getPanel(), OVButton, AddNewButton, SettButton);
 					panel = 0;
 				}
 			}
@@ -52,8 +65,7 @@ public class MainGUI {
 		// Button toggles add new
 		AddNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				buttonClicked(addNewGUI.getPanel());
-				unclickableButton(AddNewButton, OVButton, SettButton);
+				buttonClicked(addNewGUI.getPanel(), AddNewButton, OVButton, SettButton);
 				panel = 1;
 			}
 		});
@@ -62,8 +74,7 @@ public class MainGUI {
 		SettButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (addNewValidation()) { // if add new doesn't have empty fields
-					buttonClicked(settGUI.getPanel());
-					unclickableButton(SettButton, OVButton, AddNewButton);
+					buttonClicked(settGUI.getPanel(), SettButton, OVButton, AddNewButton);
 					panel = 2;
 				}
 			}
@@ -85,22 +96,19 @@ public class MainGUI {
 	}
 
 	// Generic form of action taken when any of the nav buttons are clicked, used above
-	private void buttonClicked(JPanel targetPanel) {
+	private void buttonClicked(JPanel targetPanel, JButton clickedButton, JButton unclickedButton1, JButton unclickedButton2) {
 		frame.getContentPane().removeAll(); // Removes everything from the frame
 		addNewGUI.resetUserFields(); // Resets the inputs if the user added info to them
 
 		NavBar(); // Adds the nav bar 
+		clickedButton.setEnabled(false); // sets this button to be unclickable
+		unclickedButton1.setEnabled(true); // makes this button clickable
+		unclickedButton2.setEnabled(true); // same as above
+		
 		frame.add(targetPanel, "span"); // adds the target panel
 
 		frame.revalidate();
 		frame.repaint();
-	}
-
-	// Sets button to be unclickable, used in actionlistener
-	private void unclickableButton(JButton clickedButton, JButton unclickedButton1, JButton unclickedButton2) {
-		clickedButton.setEnabled(false);
-		unclickedButton1.setEnabled(true);
-		unclickedButton2.setEnabled(true);
 	}
 
 	// Validation for addnew
@@ -118,7 +126,41 @@ public class MainGUI {
 		return valid;
 	}
 	
-	// Need to make validation for the settings
+	// Need to make validation for the settings (can do it in the same method as addNewValidation)
+	
+	// Deserialization - retrieves linkedList from list.ser
+	private LinkedList<Bill> retrieveFromFile(){
+		
+		try {
+			FileInputStream file = new FileInputStream("list.ser");
+			ObjectInputStream in = new ObjectInputStream(file);
+			listOfBills = (LinkedList<Bill>) in.readObject();
+			
+			in.close();
+			file.close();
+		} catch (IOException i){ // Exception occurs when the SER file is empty
+		} catch (ClassNotFoundException c){
+			c.printStackTrace();
+		}
+		
+		return listOfBills;
+		
+	}
+	
+	// Serialization - saves linkedList to list.ser
+	private void saveList() {
+		try {
+			FileOutputStream file = new FileOutputStream("list.ser");
+			ObjectOutputStream out = new ObjectOutputStream(file);
+			
+			out.writeObject(listOfBills);
+			
+			out.close();
+			file.close();			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String args[]) {
 		new MainGUI();
