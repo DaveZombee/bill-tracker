a1982291fecaa28b4cef19df029d01faa047ca82
DaveZombee <20luky301@gmail.com>
2021-08-16 21:20:43 +0800

    Update OverviewGUI.java

--- OverviewGUI.java
+++ OverviewGUI.java
@@ -2,9 +2,8 @@ import java.awt.Font;
 import java.util.LinkedList;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
+import javax.swing.event.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
 
 import net.miginfocom.swing.MigLayout;
@@ -16,7 +15,7 @@ public class OverviewGUI {
 	// Things for table (these are created later)
 	private JTable actualTable;
 	private JScrollPane billTable;
-	
+	private DefaultTableModel model;
 	private String[] columnHeaders = { "Type", "Notes", "Pay period", "Due date","Paid" }; // Table column headings
 	
 	// Menu which appears on right clicking a row
@@ -24,13 +23,16 @@ public class OverviewGUI {
 	
 	private JMenuItem viewItem = new JMenuItem("View");
 	private JMenuItem editItem = new JMenuItem("Edit");
-	private JMenuItem paidItem = new JMenuItem("Mark as paid");
+	private JMenuItem paidItem = new JMenuItem("Toggle paid");
 	private JMenuItem deleteItem = new JMenuItem("Delete");
+	
+	private JLabel deleteWarning = new JLabel();
 	
 	// Constructor for the overview GUI
 	public OverviewGUI(LinkedList<Bill> listOfBills) {
 		// Creating the JTable here because it needs a linked list
-		actualTable = new JTable(fillTable(listOfBills), columnHeaders);
+		model = new DefaultTableModel(fillTable(listOfBills),columnHeaders); // used so that a row can be deleted from table
+		actualTable = new JTable(model);
 		billTable = new JScrollPane(actualTable); // Lets the column headings can be seen
 	
 		// Table settings
@@ -77,41 +79,61 @@ public class OverviewGUI {
 		viewItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int selRow = actualTable.getSelectedRow();
-				String selBill = listOfBills.get(selRow).getType();
-				
-				System.out.println(selBill);
-			}
+				new ViewGUI(listOfBills.get(selRow));			
+				}
 		});
 		
 		// What happens when editItem is clicked
 		editItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFrame editFrame = new JFrame("edit");
-				editFrame.setVisible(true);
+
 			}
 		});
 		
 		// What happens when paidItem is clicked
 		paidItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				int selRow = actualTable.getSelectedRow();
 				
+				if (listOfBills.get(selRow).getPaid() == true) {
+					listOfBills.get(selRow).setPaid(false);
+					actualTable.getModel().setValueAt("No", selRow, 4);
+				}
+				else {
+					listOfBills.get(selRow).setPaid(true);
+					actualTable.getModel().setValueAt("Yes", selRow, 4);					
+				}
+				
+				actualTable.repaint(); // updates table
 			}
 		});
 		
 		// What happens when deleteItem is clicked
 		deleteItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				int selRow = actualTable.getSelectedRow();
 				
+				// warning message label
+				deleteWarning.setText("Are you sure you want to delete "+listOfBills.get(selRow).getType()+"?");
+				deleteWarning.setFont(menuFont);
+				
+				int option = JOptionPane.showConfirmDialog(panel, deleteWarning);
+			
+				if (option == JOptionPane.OK_OPTION) {
+					listOfBills.remove(selRow);
+					((DefaultTableModel) actualTable.getModel()).removeRow(selRow);
+					actualTable.repaint();
+				}
 			}
 		});
 	}
-
 	
 	// Lets MainGUI access this panel of GUI
 	public JPanel getPanel() {
 		return panel;
 	}
 	
+	// Fills in the table data
 	private String[][] fillTable(LinkedList<Bill> listOfBills) {
 		int listLength = listOfBills.size();
 		
@@ -124,5 +146,4 @@ public class OverviewGUI {
 		return tableData;
 	}
 	
-
 }
