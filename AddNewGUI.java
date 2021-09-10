7aeec5c4b9625d31dbc4ddeb74c3b8e6de26bdeb
DaveZombee <20luky301@gmail.com>
2021-09-10 13:45:46 +0800

    Update AddNewGUI.java

--- AddNewGUI.java
+++ AddNewGUI.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.util.Date;
 import java.util.LinkedList;
-import java.awt.Font;
 import java.awt.event.*;
 import java.time.LocalDate;
 
@@ -9,7 +8,7 @@ public class AddNewGUI extends InputGUI {
 
 	// Constructor for the add new GUI
 	public AddNewGUI(LinkedList<Bill> listOfBills) {
-		super(); // Inheriting the input fields and the associated texts
+		super(); // Inheriting the input fields, the associated texts and the button
 		getSaveButton().setText("Add bill"); // Set text of the button
 
 		// Actions when the add button is clicked
@@ -21,12 +20,12 @@ public class AddNewGUI extends InputGUI {
 				String selNotes = getNotesField().getText();
 				Date selDay = (Date) getDueDatePicker().getModel().getValue();
 
-				if (saveButtonClicked(selType,selDay)) {
+				if (saveButtonClicked(selType, selDay)) {
 					// Extracting the date, month and year individually to create a LocalDate
 					int selDate = selDay.getDate();
 					int selMonth = (selDay.getMonth() + 1); // Adding the number fixes the month
 					int selYear = (selDay.getYear() + 1900); // ^^
-					
+
 					LocalDate localDueDate = LocalDate.of(selYear, selMonth, selDate);
 
 					// Adding the bill to the list
@@ -36,25 +35,24 @@ public class AddNewGUI extends InputGUI {
 					getDialogLabel().setText("New bill added");
 					JOptionPane.showMessageDialog(getPanel(), getDialogLabel());
 
-					resetUserFields(); // Resets fields to be empty
+					// Resetting the user fields to be empty
+					resetUserFields();
 				}
 			}
 		});
 	}
 
-	// Resets all the different inputs to be empty
+	// Resets the user fields to their defaults (used here and in MainGUI)
 	public void resetUserFields() {
-		Integer resetSpinner = 1; // used for JSpinner, because it wants an object
-
 		getTypeField().setText("");
-		getPayPeriodPicker().setValue(resetSpinner);
+		getPayPeriodPicker().setValue(1);
 		getDueDatePicker().getJFormattedTextField().setText(""); // Makes text field empty
 		getDueDatePicker().getModel().setValue(null); // Makes the actual datepicker null
 		getNotesField().setText("");
 	}
 
 	// Validating (used at the nav buttons) - if user has input info still in the fields
-	public boolean inputsEmpty() {
+	public boolean isInputsEmpty() {
 		boolean empty = true;
 
 		// Getting the inputs
@@ -63,13 +61,11 @@ public class AddNewGUI extends InputGUI {
 		String selNotes = getNotesField().getText();
 		Date selectedDay = (Date) getDueDatePicker().getModel().getValue();
 
-		// if typefield isn't empty or pay picker isn't one or notesfield isn't empty or
-		// selected day isn't null
+		// if typefield isn't empty or pay picker isn't one or notesfield isn't empty or selected day isn't null
 		if (selType.isEmpty() == false || selPayPeriod != 1 || selNotes.isEmpty() == false || selectedDay != null) {
 			empty = false;
 		}
 
 		return empty;
 	}
-
 }
