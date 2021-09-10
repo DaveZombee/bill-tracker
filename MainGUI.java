0a551112043d39c72e932c0be4f996a5fdb0075e
DaveZombee <20luky301@gmail.com>
2021-09-10 13:46:10 +0800

    Update MainGUI.java

--- MainGUI.java
+++ MainGUI.java
@@ -10,12 +10,16 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.time.LocalDate;
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class MainGUI {
 
 	private JFrame frame = new JFrame("Bill tracker");
 
-	private LinkedList<Bill> listOfBills = retrieveFromFile(); // deserializes linkedlist from file
+	private Object[] saveList = retrieveFromFile(); // deserializing the two things from the file (below)
+
+	private LinkedList<Bill> listOfBills = (LinkedList<Bill>) saveList[0]; // getting the deserialized linkedlist
+	private ArrayList<Integer> remindList = (ArrayList<Integer>) saveList[1]; // getting the deserialized arraylist
 
 	// Navigation bar buttons
 	private JButton ovButton = new JButton("Overview");
@@ -25,14 +29,13 @@ public class MainGUI {
 	// Creating the GUIs which allows for validation
 	private OverviewGUI overviewGUI = new OverviewGUI(listOfBills);
 	private AddNewGUI addNewGUI = new AddNewGUI(listOfBills);
-	private SettGUI settGUI = new SettGUI();
+	private SettGUI settGUI = new SettGUI(remindList);
 
 	private int panel = 0; // also for validation - to see which panel is currently visible (0 = overview)
 
-	private JLabel confirmLabel = new JLabel("You will lose your unsaved info. Do you want to proceed?");
+	private JLabel confirmLabel = new JLabel("You will lose your unsaved info. Do you wish to proceed?");
 
 	private Font buttonFont = new Font(null, Font.PLAIN, 24); // font for the nav buttons
-	private Font confirmFont = new Font(null, Font.PLAIN, 16); // font for the confirmation
 
 	// Creating the main page
 	public MainGUI() {
@@ -41,40 +44,42 @@ public class MainGUI {
 //			System.out.println("The date is today 0o0"); // positive = future, negative = past, 0 = today
 //		}
 		//////////////////////////////////////////////////////////////////////
-		
+
 		// Frame layout
 		frame.setLayout(new MigLayout("", "[200][200][200]", ""));
-		
+
 		NavBar(); // adding in the nav bar
-		
+
 		frame.add(overviewGUI.getPanel(), "span"); // Starting GUI, when program is first opened
-	
+
 		frame.pack();
 		frame.setVisible(true);
-		
+
 		// Actions taken when user wants to close program
 		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE); // don't close
-		
+
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent) {
-				if (addNewValidationCloser() == 0) { // if add new doesn't have empty fields
-					saveList();  // Serializes list
+				// if add new doesn't have empty fields and settings fields aren't changed
+				if (addNewValidation() == 0 && settingsValidation() == 0) {
+					saveList(); // Serializes list
 					System.exit(0); // closes and stops program
-				}		
+				}
 			}
 		});
-		
+
 		/////////////////////////////
 		// BUTTON ACTION LISTENERS //
 		/////////////////////////////
-		
+
 		// Button toggles overview
 		ovButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (addNewValidationCloser() == 0) { // if add new doesn't have empty fields
+				// if add new doesn't have empty fields and settings fields aren't changed
+				if (addNewValidation() == 0 && settingsValidation() == 0) {
 					buttonClicked(overviewGUI.getPanel(), ovButton, addNewButton, settButton);
 					overviewGUI.reloadTable(listOfBills); // updates jtable
-					
+
 					panel = 0;
 				}
 			}
@@ -83,15 +88,18 @@ public class MainGUI {
 		// Button toggles add new
 		addNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				buttonClicked(addNewGUI.getPanel(), addNewButton, ovButton, settButton);
-				panel = 1;
+				// if settings fields haven't been changed
+				if (settingsValidation() == 0) {
+					buttonClicked(addNewGUI.getPanel(), addNewButton, ovButton, settButton);
+					panel = 1;
+				}
 			}
 		});
 
 		// Button toggles settings
 		settButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (addNewValidationCloser() == 0) { // if add new doesn't have empty fields
+				if (addNewValidation() == 0) { // if add new doesn't have empty fields
 					buttonClicked(settGUI.getPanel(), settButton, ovButton, addNewButton);
 					panel = 2;
 				}
@@ -114,10 +122,10 @@ public class MainGUI {
 	}
 
 	// Generic form of action taken when any of the nav buttons are clicked, used above
-	private void buttonClicked(JPanel targetPanel, JButton clickedButton, JButton unclickedButton1,
-			JButton unclickedButton2) {
+	private void buttonClicked(JPanel targetPanel, JButton clickedButton, JButton unclickedButton1, JButton unclickedButton2) {
 		frame.getContentPane().removeAll(); // Removes everything from the frame
-		addNewGUI.resetUserFields(); // Resets the inputs if the user added info to them
+		addNewGUI.resetUserFields(); // Resets the inputs in add new if the user added info to them
+		settGUI.resetFields(remindList); // resets the inputs back to saved settings if user changed it
 
 		NavBar(); // Adds the nav bar
 		clickedButton.setEnabled(false); // sets this button to be unclickable
@@ -131,49 +139,67 @@ public class MainGUI {
 	}
 
 	// Validation for addnew
-	private int addNewValidationCloser() {
+	private int addNewValidation() {
 		int option = 0; // zero means close
 
 		// if panel is addnew and inputs aren't empty
-		if (panel == 1 && addNewGUI.inputsEmpty() == false) {
-			confirmLabel.setFont(confirmFont);
+		if (panel == 1 && addNewGUI.isInputsEmpty() == false) {
+			confirmLabel.setFont(new Font(null, Font.PLAIN, 16));
 			option = JOptionPane.showConfirmDialog(frame, confirmLabel);
+		}
+		return option;
+	}
 
+	// Validation for settings
+	private int settingsValidation() {
+		int option = 0; // zero = close
+
+		// if panel is settings and inputs were changed
+		if (panel == 2 && settGUI.isChanged(remindList)) {
+			confirmLabel.setFont(new Font(null, Font.PLAIN, 16));
+			option = JOptionPane.showConfirmDialog(frame, confirmLabel);
 		}
 
 		return option;
 	}
 
-	// Need to make validation for the settings (could do it in the same method as addNewValidation)
-
 	// Deserialization - retrieves linkedList from list.ser
-	private LinkedList<Bill> retrieveFromFile() {
+	private Object[] retrieveFromFile() {
 
 		LinkedList<Bill> listOfBills = new LinkedList<Bill>();
+		ArrayList<Integer> remindList = new ArrayList<Integer>(2);
+		Object[] savedList = new Object[] { listOfBills, remindList };
 
 		try {
 			FileInputStream file = new FileInputStream("list.ser");
 			ObjectInputStream in = new ObjectInputStream(file);
-			listOfBills = (LinkedList<Bill>) in.readObject();
+
+			savedList = (Object[]) in.readObject();
 
 			in.close();
 			file.close();
+
 		} catch (IOException i) { // Exception occurs when the SER file is empty
 		} catch (ClassNotFoundException c) {
 			c.printStackTrace();
 		}
 
-		return listOfBills;
+		if (remindList.isEmpty()) { // if the program is being started for the first time
+			remindList.add(1);
+			remindList.add(3);
+		}
 
+		return savedList;
 	}
 
-	// Serialization - saves linkedList to list.ser
+	// Serialization - saves linkedList and remindList to list.ser
 	private void saveList() {
 		try {
 			FileOutputStream file = new FileOutputStream("list.ser");
 			ObjectOutputStream out = new ObjectOutputStream(file);
 
-			out.writeObject(listOfBills);
+			Object[] savedList = new Object[] { listOfBills, remindList };
+			out.writeObject(savedList);
 
 			out.close();
 			file.close();
